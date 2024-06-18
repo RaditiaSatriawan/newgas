a4b5e281480cbd6a0a7f9bc7a39fda73688a41d2
RaditiaSatriawan <raditiaoffice@gmail.com>
2024-06-19 07:52:13 +0800

    login signup fix 2

--- app.vue
+++ app.vue
@@ -5,6 +5,8 @@
       <ul>
         <li><NuxtLink to="/todo">To Do</NuxtLink></li>
         <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/login">Login</NuxtLink></li>
+        <li><NuxtLink to="/signup">Signup</NuxtLink></li>
       </ul>
     </nav>
     </header>
