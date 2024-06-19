3cfad9cddaf8692914b2e9a41792443d57ab3f1d
Zack09Dezel <edo09ozymandias@gmail.com>
2024-06-19 23:00:36 +0800

    Update:     v1.01

--- pages/login.vue
+++ pages/login.vue
@@ -27,29 +27,29 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useNuxtApp } from '#app';
-
+  
 const email = ref('');
 const password = ref('');
 const router = useRouter();
-
+  
 const { $firebase } = useNuxtApp();
-
+  
 const login = async () => {
   try {
     if (!validateEmail(email.value) || !validatePassword(password.value)) {
       alert('Invalid email or password (min. 6 characters)');
       return;
     }
-
+  
     await $firebase.auth.signInWithEmailAndPassword(email.value, password.value);
-
+  
     const user = $firebase.auth.currentUser;
-
+  
     if (user) {
       await $firebase.database.ref('users/' + user.uid).update({
         lastLogin: Date.now(),
       });
-
+  
       // Redirect to todo page after login
       router.push('/todo');
     }
@@ -58,7 +58,7 @@ const login = async () => {
     alert('Login error: ' + error.message);
   }
 };
-
+  
 const validateEmail = (email: string) => /^[^@]+@\w+(\.\w+)+\w$/.test(email);
 const validatePassword = (password: string) => password.length >= 6;
 </script>
